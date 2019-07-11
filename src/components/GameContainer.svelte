9df27fd348e187e693db816c75e6b63cf1063a1d
admls <adam.the.sherwood@gmail.com>
2019-07-11 08:06:26 +0100

    init GameContainer with board

---
+++ src/components/GameContainer.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let x = 100;
+  export let y = 100;
+
+  const board = new Array(y).fill(new Array(x).fill(0));
+
+  console.log(board);
+</script>
+
+<section class="game-container"></section>
+
+<style>
+  .game-container {
+    border: solid 1px black;
+  }
+</style>
