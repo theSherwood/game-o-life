5a63e1ecadbc1725e17555938b958fa59aeab995
admls <adam.the.sherwood@gmail.com>
2019-07-11 09:01:32 +0100

    init game of life helpers

---
+++ src/helpers/gameOfLife.js
@@ -0,0 +1,58 @@
+const timeStep = board => {
+  const newBoard = board.map((row, i) =>
+    row.map((cell, j) => checkCell(i, j, board))
+  );
+  return newBoard;
+};
+
+const checkCell = (i, j, board) => {
+  if (board[i][j] === 0) {
+    return handleDeadCell(i, j, board);
+  } else {
+    return handleLiveCell(i, j, board);
+  }
+};
+
+const countLiveNeighbors = (i, j, board) => {
+  let count = 0;
+  if (i > 0) {
+    count += board[i - 1][j];
+    if (j > 0) {
+      count += board[i - 1][j - 1];
+    }
+    if (j < board[0].length - 2) {
+      count += board[i - 1][j + 1];
+    }
+  }
+  if (i < board.length - 2) {
+    count += board[i + 1][j];
+    if (j > 0) {
+      count += board[i + 1][j - 1];
+    }
+    if (j < board[0].length - 2) {
+      count += board[i + 1][j + 1];
+    }
+  }
+  if (j > 0) {
+    count += board[i][j - 1];
+  }
+  if (j < board[0].length - 2) {
+    count += board[i][j + 1];
+  }
+  return count;
+};
+
+const handleDeadCell = (i, j, board) => {
+  if (countLiveNeighbors(i, j, board) === 3) {
+    return 1;
+  }
+  return 0;
+};
+
+const handleLiveCell = (i, j, board) => {
+  const count = countLiveNeighbors(i, j, board);
+  if (count === 2 || count === 3) {
+    return 1;
+  }
+  return 0;
+};
