0f1d1163d4457fa0983fde94db6a17aadfcb371f
admls <adam.the.sherwood@gmail.com>
2019-07-11 09:51:51 +0100

    add seed, faster, slower, emptyBoard functions

--- src/components/GameContainer.svelte
+++ src/components/GameContainer.svelte
@@ -1,17 +1,21 @@
 <script>
-  import {timeStep} from '../helpers/gameOfLife';
+  import {timeStep, randomSeed} from '../helpers/gameOfLife';
 
-  export let x = 100;
-  export let y = 100;
-  export let cellSize = 8;
-  export let stepSize = 100;
-
-  let board = new Array(y).fill(0)
-  board = board.map(val => {
-    return new Array(x).fill(0)
-  })
-
+  let x = 100;
+  let y = 100;
+  let cellSize = 8;
+  let stepSize = 500;
   let intervalId;
+  let board = new Array(y).fill(0)
+
+  emptyBoard()
+
+  function emptyBoard() {
+    board = board.map(val => {
+      return new Array(x).fill(0)
+    })
+  }
+  
   function run() {
     intervalId = setInterval(() => {
       board = timeStep(board);
@@ -23,9 +27,29 @@
   }
 
   function handleClick(i, j) {
-      board[i][j] = board[i][j] === 0 ? 1 : 0;
-      board = board;
+    board[i][j] = board[i][j] === 0 ? 1 : 0;
+    board = board;
+  }
+
+  function seed() {
+    board = randomSeed(board);
+  }
+
+  function faster() {
+    if (stepSize > 50) {
+      pause()
+      stepSize = Math.round(stepSize/2)
+      run()
     }
+  }
+
+  function slower() {
+    if (stepSize < 1000) {
+      pause()
+      stepSize = stepSize * 2
+      run()
+    }
+  }
 
 </script>
 
@@ -40,6 +64,10 @@
 </section>
 <button on:click={run}>Run</button>
 <button on:click={pause}>Pause</button>
+<button on:click={seed}>Seed</button>
+<button on:click={faster}>Faster</button>
+<button on:click={slower}>Slower</button>
+<button on:click={emptyBoard}>Clear</button>
 
 <style>
   .game-container :global(*) {
