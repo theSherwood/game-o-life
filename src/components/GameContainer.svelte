2ceffd4fd375f439536b21cd9717725d39c0d13b
admls <adam.the.sherwood@gmail.com>
2019-07-11 08:25:28 +0100

    renders game board

--- src/components/GameContainer.svelte
+++ src/components/GameContainer.svelte
@@ -1,16 +1,42 @@
 <script>
   export let x = 100;
   export let y = 100;
+  export let cellSize = 8;
 
   const board = new Array(y).fill(new Array(x).fill(0));
 
-  console.log(board);
 </script>
 
-<section class="game-container"></section>
+<section class="game-container">
+  {#each board as row, i}
+    <div class="row" style={`height: ${cellSize}px;`}>
+      {#each row as cell, j}
+        <div class="cell" style={`width: ${cellSize}px; height: ${cellSize}px;`}></div>
+      {/each}
+    </div>
+  {/each}
+
+</section>
 
 <style>
+  .game-container :global(*) {
+    box-sizing: border-box;
+  }
+
   .game-container {
     border: solid 1px black;
+    width: max-content;
+    height: max-content;
+  }
+
+  .row {
+    width: max-content;
+    line-height: 0;
+  }
+
+  .cell {
+    background: #333;
+    border: solid 1px black;
+    display: inline-block;
   }
 </style>
