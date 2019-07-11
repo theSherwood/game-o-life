af71ad6f940d775bb7744c1508fff406769c42ff
admls <adam.the.sherwood@gmail.com>
2019-07-11 10:58:45 +0100

    position content and change background

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,18 @@
 	import GameContainer from './components/GameContainer.svelte';
 </script>
 
+<main>
 <GameContainer />
+</main>
 
 <style>
   :global(body) {
     padding: 0;
     margin: 0;
+    background: darksalmon;
+  }
+
+  main {
+    margin-top: 20px;
   }
 </style>
